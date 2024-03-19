58bb7627075aa8def9a1768e7d3396963da832ff
Saadullahkhoso <164096006+Saadullahkhoso@users.noreply.github.com>
2024-03-20 03:40:54 +0500

    Add files via upload

---
+++ operatorstask.py
@@ -0,0 +1,24 @@
+name=("Hello!")
+plus=("I am Saadullah")
+print(name+plus) # Concatination of two string variables
+
+
+decimal=13
+multi=13
+print( f"Age is {decimal+multi}") # Concatination of two int variables
+
+graduate=2021
+year=1
+print(f"Iam a Gradute from sindh university jamshoro in {graduate*year}") # Multiply of two int variables
+
+start=4036
+end= 2018
+lifes=4042
+lifee=2021
+print(f"Start a Univerity life from {start-end} to {lifes-lifee}") # Subtraction of four int variables
+
+
+four=4
+div=1
+print(f"I have completed my{four/div}year degree") # divide of two int variables output is decimal value
+
